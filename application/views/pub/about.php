8aea568be543e71d404feadf532df634c81ecef3
slick2 <carey.dayrit@gmail.com>
2016-03-14 10:22:13 +0800

    fixing typo error

--- application/views/pub/about.php
+++ application/views/pub/about.php
@@ -12,7 +12,7 @@
 <p><b>The</b> I AM, Redeemer and Master Evangelical Church, Inc., is a Christ-centered, Bible-based and Spirit-led organization  that seeks to make significant contribution to a pervasive change in the person's spiritual moral, social and economic life and be catalyst of change in the
 redemption and transformation of nations.</p>
 <br />
-<p><b>We</b> further believe that the lcoal church is God's instrument in fulfilling Jesus' global evangelistic mission and that all true believers are co-laborers in the Kingdom of God.  We shall endeavor to equip every believer for the works of the ministry in attaining doctrinal stability, spiritual maturity and corporate unity.
+<p><b>We</b> further believe that the local church is God's instrument in fulfilling Jesus' global evangelistic mission and that all true believers are co-laborers in the Kingdom of God.  We shall endeavor to equip every believer for the works of the ministry in attaining doctrinal stability, spiritual maturity and corporate unity.
 </p>
 <br />
 <p><b>We therefore declare</b> that we, in the I AM Redeemer and Master Evangelical Church, Inc. through the guidance of the Almighty God shall endeavor to humbly present before God a glorious church, without stain or wrinkle, or any other blemish until Jesus comes.
